3fee82c05fb95ba2166c01775cadfaf5f77c37ee
louissmith <louis.smith88@gmail.com>
2022-05-25 16:02:22 -0500

    got most of Jobber's setup requirements done.

--- md_gen/openmm_jobber.py
+++ md_gen/openmm_jobber.py
@@ -1,15 +1,42 @@
-from openmm.app import *
-from openmm import *
-from openmm.unit import *
+from openmm.app import Simulation, PDBxFile, DCDReporter, StateDataReporter
+from openmm import XmlSerializer, Platform
+from pathlib import Path
 import json
 from sys import argv
-import mdtraj as md
 
 with open(argv[1]) as f:
     conf = json.load(f)
 
-sim = simulation.loadState(conf['sim'])
-pos = md.load(conf['crds'])
-sim.context.setPositions(pos.coords)
-sim.minimizeEnergy()
-sim.step(conf['n_steps'])
+
+
+# load the system components
+topology = PDBxFile(conf['topology']).getTopology()
+integrator = XmlSerializer.deserialize(Path(conf['integrator']).read_text())
+system = XmlSerializer.deserialize(Path(conf['system']).read_text())
+simulation = Simulation(topology, system, integrator, Platform.getPlatformByName(conf['platform name']),
+                        conf['platform properties'])
+simulation.loadState(conf['state'])
+# set up reporters
+steps = conf['steps']
+write_freq = conf['write_freq']
+out_prefix = conf['output_prefix']
+simulation.reporters.append(
+    DCDReporter(
+        out_prefix+'.dcd',
+        write_freq,
+        append=conf['append']
+    )
+)
+simulation.reporters.append(
+    StateDataReporter(
+        out_prefix+'.out',
+        write_freq,
+        **(conf['out reporters']),
+    )
+)
+
+# Get new velocities at specified temp
+simulation.context.setVelocitiesToTemperature(conf['temperature'])
+
+simulation.step(steps)
+
